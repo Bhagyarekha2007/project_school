d5bde7a719fb012f27c01f19ebf0fb20d3f1b236
Bhagya Rekha <bhagyarekha660@gmail.com>
2026-07-22 12:46:51 +0530

    Addded

---
+++ my-first-react-app/src/student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <div>
+      <h2>Student Details</h2>
+      <p>Name: Bhagya Rekha</p>
+      <p>Course: CSE</p>
+    </div>
+  );
+}
+
+export default Student;
